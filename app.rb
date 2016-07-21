474943f41e3c78112b73835aadf9fc06ce96a6bf
Sebastian Sim <phistando@yahoo.com>
2016-07-21 09:53:17 +0800

    heroku test

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 class RedmarkSinatraApp < Sinatra::Base
   get '/' do
-      erb '<h2>Redmark Sinatra</h2>'
+      erb '<h2>Redmark Sinatra Heroku</h2>'
     end
 
     get '/users' do
-      
+
       @users = User.all
       erb :'users'
     end
